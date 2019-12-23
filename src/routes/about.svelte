62540a4a3ef21bbdb94b05a2cb17918ea69f512e
Dhayanandhan <dhayaec@gmail.com>
2019-12-24 00:00:00 +0530

    init sapper app

---
+++ src/routes/about.svelte
@@ -0,0 +1,17 @@
+<script>
+  function add(a, b) {
+    return a + b;
+  }
+
+  let result = add(5, 6);
+</script>
+
+<svelte:head>
+  <title>About</title>
+</svelte:head>
+
+<h1>About this site</h1>
+
+<p>This is the 'about' page. There's not much here.</p>
+
+<p>{result}</p>
